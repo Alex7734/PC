0ad73d35d0ef9c46e793c10a884af43dd347d4ba
Alex7734 <63779353+Alex7734@users.noreply.github.com>
2022-11-20 18:35:14 +0200

    Update readStringFromKeyboard.c

--- readStringFromKeyboard.c
+++ readStringFromKeyboard.c
@@ -14,22 +14,21 @@ char *readStringFromKeyboard(void) {
     char c;
     while (scanf("%c", &c)) {
         if (c == '\n') {
-            /* PRIMUL CAZ DOAR CU IF UL ASTA TRECE */
-            if (i < 2 && result[i]!=0) {
+            if (i == 0) {
                 setErrorInfo(INVALID_RANGE);
                 return NULL;
             }
             break;
         }
 
-        if (i >= size){
+        if (i == size){
             size *= 2;
             result = realloc(result, size);
         }
 
         result[i++] = c;
+        result[i] = '\0';
     }
-    /* Nu conteaza daca copiez memoria din result doar cat trebuie - returnez mereu ciudatenii la final */
     char *translate = malloc(size);
     memcpy(translate, result, size);
     setErrorInfo(OK);
@@ -37,14 +36,18 @@ char *readStringFromKeyboard(void) {
 }
 
 /*
-exemplu de eroare in log:
 
-Calling readStringFromKeyboard with {"input":"012"}
-	expected: {"errorInfo":OK, "return":"012"}
-	found   : {"errorInfo":OK, "return":"01223456789 123456789 12345678""}
+Testing readStringFromKeyboard: FAIL. Test results: 500 out of 501 passed.
 
-Calling readStringFromKeyboard with {"input":"0123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 12345"}
-	expected: {"errorInfo":OK, "return":"0123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 12345"}
-	found   : {"errorInfo":OK, "return":"0123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123459 123456789 123456789 "
+
+??????????????
+???  DE CE ???
+??????????????
+
+Calling readStringFromKeyboard with {"input":"0123456789 123456789 123456789 1"}
+	expected: {"errorInfo":OK, "return":"0123456789 123456789 123456789 1"}
+	found   : {"errorInfo":OK, "return":"0123456789 123456789 123456789 1}"}"
+
+
 
 */
